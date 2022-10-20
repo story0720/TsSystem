4ba63341d9edae176651fc048fed22f11afeb760
MissyShen <love0912971992@gmail.com>
2022-10-20 23:01:30 +0800

    update bug

--- resources/views/Processing/edit.blade.php
+++ resources/views/Processing/edit.blade.php
@@ -12,7 +12,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active"><a href="{{Route('processing.index')}}">加工管理</a></li>
+                        <li class="breadcrumb-item active"><a href="{{Route('processing.index')}}">加工列表</a></li>
                         <li class="breadcrumb-item active">編輯加工</li>
                     </ol>
                 </div><!-- /.col -->
--- resources/views/Processing/index.blade.php
+++ resources/views/Processing/index.blade.php
@@ -8,12 +8,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">加工管理</h1>
+                    <h1 class="m-0">加工列表</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="/">Home</a></li>
-                        <li class="breadcrumb-item active">加工管理</li>
+                        <li class="breadcrumb-item active">加工列表</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
