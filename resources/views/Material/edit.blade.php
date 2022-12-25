698143738a852e700402838e111bdc710388c3d3
MissyShen <33143763+MissyShen@users.noreply.github.com>
2022-12-25 15:58:16 +0800

    《材料列表、新增、編輯》更新（新增材料規格與單價欄位、資料陣列加入hidden）

--- resources/views/Material/edit.blade.php
+++ resources/views/Material/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('Layout.index')
-@section('title', '新增材料《鐵祥企業》')
+@section('title', '編輯材料《鐵祥企業》')
 @section('content')
     <div class="content-wrapper">
         @if ($errors->any())
@@ -16,13 +16,13 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">新增材料</h1>
+                        <h1 class="m-0">編輯材料</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="/">Home</a></li>
                             <li class="breadcrumb-item"><a href="">材料管理</a></li>
-                            <li class="breadcrumb-item active">新增材料</li>
+                            <li class="breadcrumb-item active">編輯材料</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -40,7 +40,7 @@
                                 <div class="form-group col">
                                     <label for="">材料名稱</label>
                                     <input type="text" class="form-control" name="" id=""
-                                        placeholder="請輸入材料名稱...">
+                                        value="" aplaceholder="請輸入材料名稱...">
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -48,9 +48,9 @@
                                     <div class="form-group mb-0">
                                         <label for="">材料規格與單價</label>
                                         <div class="input-group">
-                                            <input type="text" class="form-control" data-type="specification"
+                                            <input type="text" class="form-control" data-type="specification" name=""
                                                 placeholder="請輸入材料規格...">
-                                            <input type="text" class="form-control" data-type="price"
+                                            <input type="text" class="form-control" data-type="price" name=""
                                                 placeholder="請輸入單價...">
                                             <span class="input-group-append">
                                                 <button type="button" control="add-specification" class="btn btn-info btn-flat rounded-right">
@@ -61,6 +61,18 @@
                                         <!-- /input-group -->
                                     </div>
                                     <div class="mt-2" id="material-specification-list">
+                                        @
+                                        <div class="materaial-specification-item alert alert-info d-inline-flex mb-0 p-0">
+                                                <button type="button" class="close text-white pl-2" data-dismiss="alert"
+                                                    aria-hidden="true" style="opacity: 1;">&times;</button>
+                                                <div class="pl-2 pr-1 py-1" style="font-size: 1.05rem;">
+                                                    <span class="materaial-specification">{ item }</span>
+                                                    <div class="ml-1 badge badge-light" style="font-size: 1.05rem;">
+                                                        <span class="materaial-price">{ price }</span>
+                                                    </div>
+                                                </div>
+                                        </div>
+                                        @
                                     </div>
                                 </div>
                             </div>
@@ -73,7 +85,7 @@
                         </div>
                         <!-- /.card-body -->
                         <div class="card-footer text-center">
-                            <input type="hidden" name="materialCreate">
+                            <input type="hidden" name="materialEdit">
                             <button type="submit" class="btn btn-primary">送出</button>
                         </div>
                     </form>
@@ -134,7 +146,7 @@
             let $memo = $('textarea[data-type="memo"]').val();
             materialList.push($memo);
 
-            $('input[name="materialCreate"]').attr("value",materialList);
+            $('input[name="materialEdit"]').attr("value",materialList);
         });
     });
 </script>
